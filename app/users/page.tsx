a67246b55a6bde4158877f5aad2deebac3b238cd
vb2910 <vb291024@gmail.com>
2024-12-11 23:13:31 +0530

    data fetching done

---
+++ app/users/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+interface User {
+    id: number;
+    name : string;
+}
+const Userspage = async () => {
+const res = await fetch('https://jsonplaceholder.typicode.com/users')
+const users: User[] = await res.json();
+  return (
+    <div>
+     <h1>Users</h1>
+     <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+     </ul>
+    </div>
+  )
+}
+
+export default Userspage
